749983541e9354c715e13d20a47ff6641e1416f3
caster-au <as0944868@gmail.com>
2021-03-27 17:35:30 +0530

    currency converter

---
+++ converter.js
@@ -0,0 +1,14 @@
+/**You are making a currency converter app.
+Create a function called convert, which takes two parameters: the amount to convert, and the rate, and returns the resulting amount.
+The code to take the parameters as input and call the function is already present in the Playground.
+Create the function to make the code work. */
+function main() {
+    var amount = parseFloat(readLine(), 10);
+    var rate = parseFloat(readLine(), 10);
+    function convert(amount,rate)
+    {
+        var ca=amount*rate;   
+        return ca; }
+    
+    console.log(convert(amount, rate));
+}
